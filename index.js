5988d767bb7c70666a818f68858d4d4766c0622f
Justin Pham <justin.tk.pham@gmail.com>
2022-06-05 17:07:53 +1000

    add CPU choice for rock, paper, or scissors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const CHOICES = ["Rock", "Paper", "Scissors"]
 
 function computerPlay() {
-
+    let random = Math.floor(Math.random() * CHOICES.length);
+    console.log(random, CHOICES[random]);
 }
+
+// computerPlay()
